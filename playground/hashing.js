622a58c2219e086e21971d45bcdd5b89c671eef6
Olgac Zaim <46277089+olgaczaim@users.noreply.github.com>
2019-02-11 16:23:47 +0300

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+//bcrypt.genSalt(10, (err, salt) => {
+//    bcrypt.hash(password, salt, (err, hash) => {
+//        console.log(hash);
+//    });
+//});
+
+var hashedPassword = '$2a$10$5BMCL29AvQDqrH5SBqRk1eDmQw7OeZgb1jt3quwqQNrY7IaCFy7NS';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+})
+
+//var data = {
+//    id: 10
+//};
+//
+//var token = jwt.sign(data,'123abc');
+//console.log(token);
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log('decoded', decoded);
 
 
 //var message = 'I am user number 3';
